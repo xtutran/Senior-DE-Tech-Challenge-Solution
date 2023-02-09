92b3759159d116bc9a3a371a23408bb22a3daa3b
xtutran <xtutran@gmail.com>
2023-02-10 00:42:52 +0700

    Feature: add unit test for the helper

---
+++ 01_Data_pipelines/test/test_helper.py
@@ -0,0 +1,43 @@
+from module import helper
+from datetime import datetime
+
+
+def test_is_valid_email():
+    assert helper.is_valid_email('Allison_Guzman@jackson-frazier.com')
+    assert helper.is_valid_email('Allison@jackson-frazier.net')
+    assert not helper.is_valid_email('Melissa_Riley@murray.biz')
+    assert not helper.is_valid_email('Allison@jackson-frazier..net')
+    assert not helper.is_valid_email('Allison@abc')
+
+
+def test_clean_birthday():
+    assert helper.clean_birthday('1986/01/10') == datetime(1986, 1, 10)
+    assert helper.clean_birthday('1974-09-10') == datetime(1974, 9, 10)
+    assert helper.clean_birthday('02/03/1974') == datetime(1974, 3, 2)
+    assert helper.clean_birthday('12/23/1974') == datetime(1974, 12, 23)
+    assert helper.clean_birthday('12.23.1974') is None
+
+
+def test_gen_membership_id():
+    assert helper.gen_membership_id(None, '19870101') is None
+    assert helper.gen_membership_id('tran', None) is None
+    assert helper.gen_membership_id('tran', '19860110') == 'tran_3864b'
+
+
+def test_clean_mobile_number():
+    assert helper.clean_mobile_number('2387 9991') == 23879991
+    assert helper.clean_mobile_number('23879991') == 23879991
+    assert helper.clean_mobile_number('a2387 9991') is None
+
+
+def test_is_valid_mobile_number():
+    assert helper.is_valid_mobile_number(23879991)
+    assert not helper.is_valid_mobile_number(2389991)
+
+
+def test_split_fullname():
+    assert helper.split_fullname("Mr. Scott Martinez") == ["Scott", "Martinez"]
+    assert helper.split_fullname("") == [None, None]
+    assert helper.split_fullname("Mr. Scott Martinez PhD") == ["Scott", "Martinez"]
+    assert helper.split_fullname("Scott Martinez PhD") == ["Scott", "Martinez"]
+    assert helper.split_fullname("Mr. Scott") == ["Scott", None]
